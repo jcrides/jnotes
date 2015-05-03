d8b92a48a82867da058a131d71f7449e44c19596
Jeremy Carey <jeremycar@gmail.com>
2015-05-02 22:29:01 -0700

    set up notebooks model, controller and basic views

---
+++ app/controllers/notebooks_controller.rb
@@ -0,0 +1,40 @@
+class NotebooksController < ApplicationController
+  def index
+    @notebooks = Notebook.all
+  end
+
+  def show
+    @notebook = Notebook.where(:id => params[:id]).first
+  end
+
+  def new
+    @notebook = Notebook.new
+  end
+
+  def create
+    @notebook = Notebook.new(notebook_params)
+    if @notebook.save
+      redirect_to @notebook
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @notebook = Notebook.where(:id => params[:id]).first
+  end
+
+  def update
+    @notebook = Notebook.where(:id => params[:id]).first
+    if @notebook.updated(notebook_params)
+      redirect_to @notebook
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def notebook_params
+    params.require(:notebook).permit(:name)
+  end
+end
---
+++ test/factories/notebooks.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :notebook do
+    name "MyString"
+user_id 1
+  end
+
+end
