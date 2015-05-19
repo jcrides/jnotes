22ea87e1d4b7494eb67a43cf61281016d6c12b9d
Jeremy <jeremycar@gmail.com>
2015-05-18 23:41:08 -0700

    configure acts_as_taggable_on and add bullet

---
+++ config/initializers/acts_as_taggable_on.rb
@@ -0,0 +1,6 @@
+# configuration file for acts_as_taggable_on gem
+
+# remove unused tags after removing taggings
+ActsAsTaggableOn.remove_unused_tags = true
+# save all tags as lower case
+ActsAsTaggableOn.force_lowercase = true
