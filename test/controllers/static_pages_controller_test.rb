f25f5fa4065943dd290bcb5ac726d5ed443d6c8f
Jeremy Carey <jeremycar@gmail.com>
2015-06-17 23:00:25 -0700

    add static_pages logged in index test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get index" do
+  test "not logged in index" do
     get :index
+    assert_response :success
+  end
+
+  test "logged in index" do
+    user = create(:user)
+    sign_in user
+
+    get :index, :user_id => user
     assert_response :success
   end
 end
