c853e130444b5f15a7ebaa6161a2809655a12235
Jeremy Carey <jeremycar@gmail.com>
2015-06-17 22:52:44 -0700

    home page shows notebooks and folders if logged in, note table shows first 81 chars

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def index
+    if current_user
+      @folders = Folder.where(:user_id => current_user.id)
+      @notebooks = Notebook.where(:user_id => current_user.id)
+    end
   end
 end
